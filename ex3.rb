36b69e7c5a16733ebf0b990a947286a95dbfc015
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-18 13:23:57 +0000

    Passing third test, no longer using inheritiance as using initialize

--- ex3.rb
+++ ex3.rb
@@ -1,15 +1,13 @@
-class Object
-  def to_be_equal(suggested_answer, correct_answer)
-    if suggested_answer == correct_answer
+class Expect
+  def initialize(suggested_answer)
+    @suggested_answer = suggested_answer
+  end
+
+  def to_equal(correct_answer)
+    if @suggested_answer == correct_answer
       "Test passes! :)"
     else
       "Test fails! D:"
     end
-  end
-end
-
-class Expect < Object
-  def initialise(suggested_answer)
-    
   end
 end
