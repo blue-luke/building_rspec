194dd54a4d2a7dac679e01c643329a894360faf8
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-18 13:39:56 +0000

    Conclusion complete, using rspec syntax by reintroducing methods defined on the object class

---
+++ conclusion.rb
@@ -0,0 +1,35 @@
+class Expect
+  def initialize(suggested_answer)
+    @suggested_answer = suggested_answer
+  end
+
+  def to(correct_answer_object)
+    if @suggested_answer == correct_answer_object.correct_answer
+      "Test passes! :)"
+    else
+      "Test fails! D:"
+    end
+  end
+end
+
+class Equal
+  attr_reader :correct_answer
+  def initialize(correct_answer)
+    @correct_answer = correct_answer
+  end
+end
+
+class Object
+  def expect(value)
+    Expect.new(value)
+  end
+
+  def eq(value)
+    Equal.new(value)
+  end
+end
+
+# > Expect.new(true).to(Equal.new(true))
+# => "Test passes! :)"
+# > Expect.new(true).to(Equal.new(false))
+# => "Test fails! D:"
