167e6f1d5a8c95dcc99edabcb1b5d5c7c5de8c88
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-18 13:33:35 +0000

    Refactored to use Expect and Equal classes

--- ex4.rb
+++ ex4.rb
@@ -1,11 +1,10 @@
 class Expect
-  attr_reader :answer
-  def initialize(answer)
-    @answer = answer
+  def initialize(suggested_answer)
+    @suggested_answer = suggested_answer
   end
 
   def to(correct_answer_object)
-    if @answer == correct_answer_object.answer
+    if @suggested_answer == correct_answer_object.correct_answer
       "Test passes! :)"
     else
       "Test fails! D:"
@@ -13,8 +12,15 @@ class Expect
   end
 end
 
+class Equal
+  attr_reader :correct_answer
+  def initialize(correct_answer)
+    @correct_answer = correct_answer
+  end
+end
 
-# > Expect.new(true).to(Expect.new(true))
+
+# > Expect.new(true).to(Equal.new(true))
 # => "Test passes! :)"
 # > Expect.new(true).to(Equal.new(false))
 # => "Test fails! D:"
