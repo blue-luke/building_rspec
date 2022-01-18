a9f5416533956d4b6abc50a7237a2af991896fa8
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-18 15:02:42 +0000

    Not sure how to add additional helpers. Logic doesn't sit comfortably between the Expect class and the helper classes

--- conclusion.rb
+++ conclusion.rb
@@ -1,10 +1,10 @@
 class Expect
   def initialize(suggested_answer)
-    @suggested_answer = suggested_answer
+    @suggested_answer = !!suggested_answer
   end
 
   def to(correct_answer_object)
-    if @suggested_answer == correct_answer_object.correct_answer
+    if @suggested_answer == correct_answer_object(correct_answer).true?
       "Test passes! :)"
     else
       "Test fails! D:"
@@ -13,12 +13,18 @@ class Expect
 end
 
 class Equal
-  attr_reader :correct_answer
   def initialize(correct_answer)
     @correct_answer = correct_answer
   end
+
+  def true?
+    
+  end
 end
 
+# class Include
+#   def initialize
+# end
 
 def expect(value)
   Expect.new(value)
@@ -28,6 +34,9 @@ def eq(value)
   Equal.new(value)
 end
 
+# def include(value)
+#   Include.new(value)
+# end
 
 # > Expect.new(true).to(Equal.new(true))
 # expect([1, 2, 3]).to include 2
